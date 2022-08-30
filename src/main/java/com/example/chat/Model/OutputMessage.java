e5e71f2c42096874f17e58d9fde6ae646aa76e0b
Floppir <dvgstadium@gmail.com>
2022-08-30 18:13:17 +0300

    update

--- src/main/java/com/example/chat/Model/OutputMessage.java
+++ src/main/java/com/example/chat/Model/OutputMessage.java
@@ -1,5 +1,10 @@
 package com.example.chat.Model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class OutputMessage {
 
     private String from;
@@ -12,27 +17,4 @@ public class OutputMessage {
         this.time = time;
     }
 
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
 }
